bc505758111b5fe80d2e281b9126abbff8c89129
Mavzoleyka <vadikdran@yandex.ru>
2025-03-28 20:57:29 +0700

    initial commit

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+
+      /*  System.out.println("""
+                 “Your time is limited,
+                  so don’t waste it
+                 living someone else’s life”
+                  Steve Jobs""");
+       */
+    }
+}
